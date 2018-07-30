c436cf7d8f97adfb1d5ab4ff60537657a292c014
U-Denis-pc\Denis <denisbelyy@gmail.com>
2018-07-30 19:54:15 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-  console.log('test');
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello! ${userName} :)\n`);
