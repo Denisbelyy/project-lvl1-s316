b8c75d9fbee299d1ce48ff0b7820e19f62a9b16d
U-Denis-pc\Denis <denisbelyy@gmail.com>
2018-07-30 16:09:33 +0300

    add readline-sync

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import name from '..';
 console.log("Welcome to the Brain Games!");
+console.log(name);
+console.log(`Hello ${name}`);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import readlyneSync from 'readline-sync';
+
+export const name = readlyneSync.question('May I have your name?');
+
