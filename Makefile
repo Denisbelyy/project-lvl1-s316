58ecd762b05ecf5fa101b5918197e7b1d3165fb3
Denisbelyy <denisbelyy@gmail.com>
2018-08-03 20:11:09 +0300

    refactoring brain-gcd

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 start:
-	npm run babel-node -- src/bin/brain-calc.js
+	npm run babel-node -- src/bin/brain-gcd.js
 publish:
 	npm publish
 lint:
--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,33 +2,19 @@ import getRandomNumber from '../random-number';
 import gamePlay from '../gamePlay';
 import { cons } from '../brain-pairs';
 
+const description = 'Find the greatest common divisor of given numbers.';
 const isDivisor = (num, divisor) => num % divisor === 0;
-const getUniqueArr = (arr) => {
-  const uniqueArr = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    for (let j = i + 1; j < arr.length; j += 1) {
-      if (arr[i] === arr[j]) {
-        uniqueArr.push(arr[j]);
-      }
+
+const searchDivisor = (firstNumber, secondNumber) => {
+  const divider = [];
+  const fewer = firstNumber < secondNumber ? firstNumber : secondNumber;
+  for (let i = 0; i < fewer; i += 1) {
+    if (isDivisor(firstNumber, i) && isDivisor(secondNumber, i)) {
+      divider.push(i);
     }
   }
-  return uniqueArr;
+  return divider[divider.length - 1];
 };
-const searchDivisor = (a, b) => {
-  const iter = (acc, count, num) => {
-    if (count === num) {
-      return acc;
-    }
-    if (isDivisor(num, count)) {
-      acc.push(count);
-    }
-    return iter(acc, count + 1, num);
-  };
-  const dividers = iter([], 0, a).concat(iter([], 0, b));
-  const uniqueDividers = getUniqueArr(dividers);
-  return uniqueDividers[uniqueDividers.length - 1];
-};
-const description = 'Find the greatest common divisor of given numbers.';
 const generatorQuestions = () => {
   const firstNum = getRandomNumber(2, 50);
   const secondNum = getRandomNumber(2, 50);
